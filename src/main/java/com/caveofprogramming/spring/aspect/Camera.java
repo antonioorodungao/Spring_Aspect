40bbcb2481d4178d4c0b512e729b51e965d2927e
antonioorodungao <antoniodungao@yahoo.com>
2016-11-12 17:10:19 +0300

    Annotation Based Aspect J

--- src/main/java/com/caveofprogramming/spring/aspect/Camera.java
+++ src/main/java/com/caveofprogramming/spring/aspect/Camera.java
@@ -1,8 +1,11 @@
 package com.caveofprogramming.spring.aspect;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by anton on 11/12/2016.
  */
+@Component
 public class Camera {
 
     public void snap(){
--- src/main/java/com/caveofprogramming/spring/aspect/Logger.java
+++ src/main/java/com/caveofprogramming/spring/aspect/Logger.java
@@ -1,9 +1,24 @@
 package com.caveofprogramming.spring.aspect;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by anton on 11/12/2016.
  */
+
+@Aspect
+@Component
 public class Logger {
+
+    @Pointcut("execution(void com.caveofprogramming.spring.aspect.Camera.snap())")
+    public void cameraSnap(){
+
+    }
+
+    @Before("cameraSnap()")
     public void aboutToTakePhoto(){
         System.out.println("About to take photo.");
     }
